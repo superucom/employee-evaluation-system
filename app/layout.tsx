97432cb080dbce1589bbb7a24fdf92414cdd27ed
superucom <minaungaung4280@gmail.com>
2026-08-30 19:35:52 +0700

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/components/providers/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,10 +15,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="th" suppressHydrationWarning>
-      <body className={inter.className}>
-        <ThemeProvider>{children}</ThemeProvider>
-      </body>
+    <html lang="th">
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
